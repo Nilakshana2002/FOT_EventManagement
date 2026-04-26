bbdfce7b8a2a1c2210934dd441003aa720c40081
Anjana Nirmal <anjanan414@gmail.com>
2026-04-26 21:27:03 +0530

    Add BookingStatusSubject class to manage booking observer notifications

---
+++ src/main/java/com/example/eventManagement/observer/BookingStatusSubject.java
@@ -0,0 +1,23 @@
+package com.fot.eventsystem.observer;
+
+import com.fot.eventsystem.model.Booking;
+import org.springframework.stereotype.Component;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Component
+public class BookingStatusSubject {
+    
+    private final List<BookingObserver> observers = new ArrayList<>();
+
+    public void addObserver(BookingObserver observer) {
+        observers.add(observer);
+    }
+
+    public void notifyObservers(Booking booking) {
+        for (BookingObserver observer : observers) {
+            observer.update(booking);
+        }
+    }
+}
