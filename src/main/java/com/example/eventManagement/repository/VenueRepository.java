93ac998ae561d695c22df786c0caac46de26506b
pasindu <5657pasindu@gmail.com>
2026-04-26 09:03:06 +0530

    delete file mistakenly created

--- src/main/java/com/example/eventManagement/repository/VenueRepository.java
+++
@@ -1,8 +0,0 @@
-package com.fot.eventsystem.repository;
-
-import com.fot.eventsystem.model.Venues;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VenueRepository extends JpaRepository<Venues, Long> {
-
-}
